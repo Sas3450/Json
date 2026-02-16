0807fbaaafef64a8c81623c93269a466f4fe1baa
szabob <szabob@310-10>
2026-02-16 10:30:55 +0100

    JsonSerrialize osztály

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,32 +1,78 @@
-﻿using System.Security.Cryptography.X509Certificates;
-using System.Text.Json.Nodes;
-using System.Text.Json.Serialization;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
 
-namespace ConsoleApp1
+namespace ConsoleApp2
 {
     class Adat
     {
         public List<string> nevek { get; set; }
         public List<int> korok { get; set; }
     }
-    class diak 
-    { 
-    public string nev { get; set; }
-    PublicKey List<int> jegyek { get; set; }
+
+    class Diakok
+    {
+        public string nev { get; set; }
+        public List<int> jegyek { get; set; }
     }
+
+    public class Munkavallalo
+    {
+        public string nev { get; set; }
+        public int fizetes { get; set; }
+        public bool jogositvany { get; set; }
+        public string munkarend { get; set; }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            string fajl = File.ReadAllText("json.json", System.Text.Encoding.Latin1);
-            Console.WriteLine(fajl);
-            Adat adat = JsonSerializableAttribute.Deserialize<Adat>(fajl);
-            foreach (var nev in adat.nevek)
+            string adatJson = File.ReadAllText("adatok.json", System.Text.Encoding.Latin1);
+            Adat adat = JsonSerializer.Deserialize<Adat>(adatJson);
+            foreach (var n in adat.nevek) Console.WriteLine(n);
+            Console.WriteLine($"{adat.nevek[0]} kora: {adat.korok[0]}");
+
+            string diakJson = File.ReadAllText("diakok.json", System.Text.Encoding.Latin1);
+            List<Diakok> diakok = JsonSerializer.Deserialize<List<Diakok>>(diakJson);
+            Console.Write("Keresett diák: ");
+            string keresett = Console.ReadLine();
+            bool van = false;
+            foreach (var d in diakok)
             {
-                Console.WriteLine(nev);
+                if (d.nev == keresett)
+                {
+                    Console.WriteLine("Átlag: " + d.jegyek.Average());
+                    van = true;
+                }
+            }
+            if (!van) Console.WriteLine("Nincs ilyen diák!");
+
+            string mFajl = ("munkavallalok.json");
+            string mJson = File.ReadAllText(mFajl);
+            List<Munkavallalo> munkavallalok = JsonSerializer.Deserialize<List<Munkavallalo>>(mJson);
+
+            munkavallalok.Add(new Munkavallalo
+            {
+                nev = "Szabó Júlia",
+                fizetes = 380000,
+                jogositvany = false,
+                munkarend = "10:00-18:00"
+            });
+
+            Console.WriteLine("Jogosítvánnyal rendelkezők:");
+            foreach (var m in munkavallalok)
+            {
+                if (m.jogositvany) Console.WriteLine("- " + m.nev);
             }
 
-            Console.WriteLine($"{adat.nevek[0]} életkora{adat.korok[0]}");
+            var opciok = new JsonSerializerOptions { WriteIndented = true };
+            string mentendo = JsonSerializer.Serialize(munkavallalok, opciok);
+            File.WriteAllText(mFajl, mentendo);
+
+            Console.WriteLine("Kész.");
         }
     }
 }
